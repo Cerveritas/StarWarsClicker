567a6f748c9871aa4d4a7cfe1bcb743088d9389f
seceji@gmail.com <seceji@gmail.com>
2024-02-28 00:31:05 +0100

    COMMIT 28/02/2024 - 0:28am
    VERSION 1.0.0 --> VERSION 1.5.0
    
    - Fondos de pantalla nuevos
    - Iconos de lado luminoso y lado oscuro
    - Funcionalidad icono lado luminoso
    - Funcionalidad puntuacion lado luminoso
    - Funcionalidad icono yoda
    - Funcionalidad icono luke
    ---------------------------------------------
    - New wallpapers
    - Light side and dark side icons
    - Light side icon functionality
    - Light side scoring functionality
    - Yoda icon functionality
    - Luke icon functionality

--- app/src/main/java/com/example/bananaclicker/MainJuego.java
+++ app/src/main/java/com/example/bananaclicker/MainJuego.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainJuego extends AppCompatActivity {
 
+    private Handler handler;
+    private Runnable runnable;
+    private Runnable runnable2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +22,107 @@ public class MainJuego extends AppCompatActivity {
 
         Switch switchModoLuminoso = findViewById(R.id.switch2);
 
+        ImageButton botonRepublica = findViewById(R.id.buttonRepublic);
+        ImageButton botonYoda = findViewById(R.id.imageButtonYoda);
+            /*botonYoda.setEnabled(false);
+            botonYoda.setAlpha(0.2f);*/
+        ImageButton botonChewa = findViewById(R.id.imageButtonChewa);
+            /*botonChewa.setEnabled(false);
+            botonChewa.setAlpha(0.2f);*/
+        ImageButton botonLuke = findViewById(R.id.imageButtonLuke);
+            /*botonLuke.setEnabled(false);
+            botonLuke.setAlpha(0.2f);*/
+
+        TextView textoPuntuacion = findViewById(R.id.textViewPoints);
+        TextView textViewChewa = findViewById(R.id.textViewChewa);
+
+
+
+
+        /********************************************************************************************************************************/
+
+
+
+
         switchModoLuminoso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainJuego.this, ModoDios.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
             }
         });
+
+        //Variables de puntuacion
+        final int[] puntuacion = {0};
+        final int puntuacionAcumulada = Integer.parseInt(textoPuntuacion.getText().toString());
+
+
+
+        botonRepublica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                puntuacion[0]++;
+                textoPuntuacion.setText(String.valueOf(puntuacion[0]));
+
+
+
+            }
+        });
+
+
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int count = Integer.parseInt(textoPuntuacion.getText().toString());
+                count++;
+                textoPuntuacion.setText(String.valueOf(count));
+                handler.postDelayed(runnable, 1000); // Suma cada 1 segundo
+
+
+            }
+        };
+
+        runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                int count = Integer.parseInt(textoPuntuacion.getText().toString());
+                count += 50;
+                textoPuntuacion.setText(String.valueOf(count));
+                handler.postDelayed(runnable2, 1000); // Suma cada 1 segundo
+            }
+        };
+
+
+        botonYoda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                handler.post(runnable);
+
+            }
+        });
+
+        botonChewa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        botonLuke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.post(runnable2);
+
+            }
+        });
+
+
+
 
 
     }
--- app/src/main/java/com/example/bananaclicker/ModoDios.java
+++ app/src/main/java/com/example/bananaclicker/ModoDios.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class ModoDios extends AppCompatActivity {
 
@@ -15,12 +17,24 @@ public class ModoDios extends AppCompatActivity {
         setContentView(R.layout.activity_modo_dios);
 
         Switch switchModoLadoOscuro = findViewById(R.id.switch1);
+        ImageButton botonImperio = findViewById(R.id.buttonImperio);
+        TextView textoPuntuacion = findViewById(R.id.textViewPoints);
 
         switchModoLadoOscuro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ModoDios.this, MainJuego.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
+            }
+        });
+
+        final int puntuacion[] = {0};
+        botonImperio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                puntuacion[0]++;
+                textoPuntuacion.setText(String.valueOf(puntuacion[0]));
             }
         });
 
